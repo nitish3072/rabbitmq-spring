25e4de1a04e4ce63fd137584ce3a3765e0129406
Nitish <nitish@enliteresearch.com>
2023-10-05 09:44:17 +0530

    Added direct exchange

---
+++ src/main/java/com/nitish/rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,57 @@
+package com.nitish.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("rabbitmq.queue")
+    String queueName;
+
+    @Value("rabbitmq.exchange")
+    String exchangeName;
+
+    @Value("rabbitmq.routing-key")
+    String directRoutingKey;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(directRoutingKey);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+}
---
+++ src/test/java/com/nitish/rabbitmq/IntegrationTest.java
@@ -0,0 +1,123 @@
+//package com.nitish.rabbitmq;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import okhttp3.mockwebserver.MockResponse;
+//import okhttp3.mockwebserver.MockWebServer;
+//import okhttp3.mockwebserver.RecordedRequest;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import reactor.core.publisher.Mono;
+//
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration
+//@TestPropertySource(locations = {"classpath:application.properties"})
+//public class IntegrationTest {
+//
+//	ObjectMapper objectMapper = new ObjectMapper();
+//
+//	public MockWebServer mockBackEnd;
+//
+//	@BeforeEach
+//	void setUp() throws IOException {
+//		mockBackEnd = new MockWebServer();
+//		mockBackEnd.start();
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws IOException {
+//		mockBackEnd.shutdown();
+//	}
+//
+//	@Test
+//	public void checkWebClientGetConnection() throws JsonProcessingException, URISyntaxException {
+//		Map<String, Object> resp = new HashMap<>();
+//		resp.put("a", 100);
+//		resp.put("b", true);
+//		resp.put("c", "Hello");
+//
+//		mockBackEnd.enqueue(new MockResponse().setResponseCode(200)
+//				.setBody(objectMapper.writeValueAsString(resp))
+//				.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//		);
+//
+//		URI uri = new URI("http", null, "localhost", mockBackEnd.getPort(),
+//				"", null, null);
+//
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//
+//		Mono<DefaultWebResultResponse<Map>> mono =  MonoCallback.createMonoOutput(HttpMethod.GET, uri, httpHeaders, new HashMap<>());
+//		DefaultWebResultResponse<Map> defaultWebResultResponse = mono.block();
+//		System.out.println(objectMapper.writeValueAsString(defaultWebResultResponse.getData()));
+//		assertEquals(defaultWebResultResponse.getData(), resp);
+//	}
+//
+//	@Test
+//	public void checkWebClientPostConnection() throws JsonProcessingException, URISyntaxException, InterruptedException {
+//
+//
+//		mockBackEnd.enqueue(new MockResponse().setResponseCode(200)
+//				.setBody((recordedRequest, bufferedSource, bufferedSink) -> bufferedSink.write(bufferedSource.readByteArray()))
+//				.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//		);
+//
+//
+//		URI uri = new URI("http", null, "localhost", mockBackEnd.getPort(),
+//				"", null, null);
+//
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//
+//		Map<String, Object> resp = new HashMap<>();
+//		resp.put("a", 100);
+//		resp.put("b", true);
+//		resp.put("c", "Hello");
+//
+//		Mono<DefaultWebResultResponse<Map>> mono =  MonoCallback.createMonoOutput(HttpMethod.POST, uri, httpHeaders, resp);
+//		DefaultWebResultResponse<Map> defaultWebResultResponse = mono.block();
+//		RecordedRequest request = mockBackEnd.takeRequest();
+//		assertEquals(objectMapper.writeValueAsString(resp), request.getBody().readUtf8());
+//	}
+//
+//	@Test
+//	public void checkWebClientPutConnection() throws JsonProcessingException, URISyntaxException, InterruptedException {
+//
+//
+//		mockBackEnd.enqueue(new MockResponse().setResponseCode(200)
+//				.setBody((recordedRequest, bufferedSource, bufferedSink) -> bufferedSink.write(bufferedSource.readByteArray()))
+//				.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//		);
+//
+//
+//		URI uri = new URI("http", null, "localhost", mockBackEnd.getPort(),
+//				"", null, null);
+//
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//
+//		Map<String, Object> resp = new HashMap<>();
+//		resp.put("a", 100);
+//		resp.put("b", true);
+//		resp.put("c", "Hello");
+//
+//		Mono<DefaultWebResultResponse<Map>> mono =  MonoCallback.createMonoOutput(HttpMethod.PUT, uri, httpHeaders, resp);
+//		DefaultWebResultResponse<Map> defaultWebResultResponse = mono.block();
+//		RecordedRequest request = mockBackEnd.takeRequest();
+//		assertEquals(objectMapper.writeValueAsString(resp), request.getBody().readUtf8());
+//	}
+//
+//}
